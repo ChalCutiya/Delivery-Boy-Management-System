32c48f2018699ca977fca74d84019cffc258e5cd
chalcutiya <anoopvipin58@gmail.com>
2024-06-01 00:50:13 +0530

    searching added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Users } from './components/Homepage/Users';
 import { SetUser } from './components/SetUser/SetUser';
 import Header from './components/Header/Header';
 import UserHistory from './components/UserHistory/UserHistory';
-import { getUndeliveredUsers } from './components/Extra/curdUser';
 
 function App() {
   const [users, setUsers] = useState([]);
 
-  const handleUsers = users => {
-    setUsers(users); 
-  };
-
-  useEffect(() => {
-    const  undeliveredUsers= getUndeliveredUsers();
-    setUsers(undeliveredUsers);
-}, []); 
-
+  const handleUsers = useCallback((users) => {
+    setUsers(users);
+  }, []);
   return (
     <Router>
       <div className="body bg-black rounded relative ">
-        <Header showUsers={handleUsers} />
+        <Header showUsers={handleUsers} users={users} />
         <Routes>
           <Route path="/" element={<Users users={users} />} />
           <Route path="/set" element={<SetUser />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,40 @@
-import React, {  useState } from 'react';
-import { Link } from 'react-router-dom';
-import { getAllUsers } from '../Extra/curdUser';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAllUsers, getUndeliveredUsers } from '../Extra/curdUser';
 
 const Header = ({ showUsers }) => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [preUsers, setPreUsers] = useState([]);
+    const navigate = useNavigate();
 
     const handleUsers = () => {
-        showUsers(getAllUsers());
-    }
+        const allUsers = getAllUsers();
+        showUsers(allUsers);
+        setPreUsers(allUsers);
+        navigate('/');
+    };
+
+    useEffect(() => {
+        const undeliveredUsers = getUndeliveredUsers();
+        showUsers(undeliveredUsers);
+        setPreUsers(undeliveredUsers);
+    }, [showUsers]);
+
     const handleSearch = () => {
-        // onSearch(searchTerm);
+        if (searchTerm.length === 0) {
+            showUsers(preUsers);
+            return;
+        }
+
+        const searchUsers = preUsers.filter(user => {
+            const nameMatch = user.name && user.name.toLowerCase().startsWith(searchTerm.toLowerCase());
+            const numberMatch = user.number && user.number.startsWith(searchTerm);
+            const addressMatch = user.address && user.address.toLowerCase().startsWith(searchTerm.toLowerCase());
+
+            return nameMatch || numberMatch || addressMatch;
+        });
+
+        showUsers(searchUsers);
     };
 
     return (
@@ -17,18 +42,16 @@ const Header = ({ showUsers }) => {
             <input
                 type="text"
                 placeholder="Search..."
-                name='search'
+                name="search"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 onKeyUp={handleSearch}
                 className="p-2 rounded-md border border-gray-400 focus:outline-none focus:ring focus:ring-gray-300"
             />
-
             <button onClick={handleUsers} className="bg-pink-800 text-white py-2 px-2 rounded-lg shadow-md">All</button>
             <Link to="/set">
                 <button className="bg-red-800 text-white py-2 px-2 rounded-lg shadow-md">Add New</button>
             </Link>
-
         </div>
     );
 };
